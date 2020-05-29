e84e76efa6c84a4078b2b6d9db1b4c8e3d6e00a2
Maxim22052002 <56323419+Maxim22052002@users.noreply.github.com>
2020-05-29 21:21:28 +0300

    Task3 Incidence matrix and list

---
+++ Number3(10)/Number3(10)/GraphMatrix.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Number3_10_
+{
+    public class GraphMatrix
+    {
+        private int Vertices = 0;
+
+        private int[,] Graph = null;
+
+        public GraphMatrix(int[,] matrix, int numberOfvertices)
+        {
+            Graph = matrix;
+            Vertices = numberOfvertices;
+        }
+
+        public Stack<int> backChain(int[] p, int startPos, int endPos)
+        {
+            int pos = endPos;
+
+            Stack<int> pathStack = new Stack<int>();
+            pathStack.Push(pos);
+
+            while (pos != startPos)
+            {
+                pos = p[pos];
+                pathStack.Push(pos);
+            }
+
+            return pathStack;
+        }
+
+        public Stack<int> DFS(int startPos, int endPos)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] vPath = new int[Vertices];
+            int[] checkedv = new int[Vertices];
+            stack.Push(startPos);
+            checkedv[startPos] = 1;
+            while (stack.Count > 0)
+            {
+                int i = stack.Pop();
+
+                for (int j = Vertices - 1; j >= 0; j--)
+                {
+                    if (Graph[i, j] > 0 && checkedv[j] == 0)
+                    {
+                        checkedv[j] = 1;
+                        stack.Push(j);
+                        vPath[j] = i;
+
+                        if (j == endPos)
+                        {
+                            return backChain(vPath, startPos, endPos);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public Stack<int> BFS(int startPos, int endPos)
+        {
+            Queue<int> q = new Queue<int>();
+            int[] vPath = new int[Vertices];
+            int[] checkedv = new int[Vertices];
+            q.Enqueue(startPos);
+            checkedv[startPos] = 1;
+            while (q.Count > 0)
+            {
+                int i = q.Dequeue();
+
+                for (int j = 0; j < Vertices; j++)
+                {
+                    if (Graph[i, j] > 0 && checkedv[j] == 0)
+                    {
+                        checkedv[j] = 1;
+                        q.Enqueue(j);
+                        vPath[j] = i;
+
+                        if (j == endPos)
+                        {
+                            return backChain(vPath, startPos, endPos);
+                        }
+                    }
+                }
+
+
+            }
+            return null;
+        }
+    }
+}
---
+++ Number3(10)/Number3(10)/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Number3_10_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] Initmatrix =
+                {
+                    {0,1,1,0,0,0,0,0}, //1
+                    {1,0,0,0,0,1,1,0}, //2
+                    {1,0,0,1,0,1,0,1}, //3
+                    {0,0,1,0,1,0,0,0}, //4
+                    {0,0,0,1,0,1,0,0}, //5
+                    {0,1,1,0,1,0,0,0}, //6
+                    {0,1,0,0,0,0,0,1}, //7
+                    {0,0,1,0,0,0,1,0}  //8
+                  // 1 2 3 4 5 6 7 8
+};
+
+            GraphMatrix Graph = new GraphMatrix(Initmatrix, 8);
+
+            int X = 0; int Y = 0;
+
+
+            Console.WriteLine("Введите вершину X:");
+
+            CheckInputCorrect(ref X);
+
+            Console.WriteLine("Введите вершину Y:");
+
+            CheckInputCorrect(ref Y);
+
+            Console.WriteLine("Граф, представленный в виде матрицы инцидентности: ");
+
+
+            Console.WriteLine("DFS: ");
+            Stack<int> DFSMatrix = Graph.DFS(X - 1, Y - 1);
+            ShowForStack(DFSMatrix);
+            Console.WriteLine();
+
+            Console.WriteLine("BFS: ");
+            Stack<int> BFSMatrix = Graph.BFS(X - 1, Y - 1);
+            ShowForStack(BFSMatrix);
+            Console.WriteLine();
+
+            
+
+            Console.WriteLine("Граф, представленный в виде связного списка: ");
+            Dictionary<int, List<int>> graph1 = new Dictionary<int, List<int>>
+            {
+                [1] = new List<int> { 2, 3 },
+                [2] = new List<int> { 1, 6, 7 },
+                [3] = new List<int> { 1, 4, 6, 8 },
+                [4] = new List<int> { 3, 5 },
+                [5] = new List<int> { 4, 6 },
+                [6] = new List<int> { 2, 3, 5 },
+                [7] = new List<int> { 2, 8 },
+                [8] = new List<int> { 3, 7 },
+            };
+            
+
+            GraphLinkedList GraphLinkedList = new GraphLinkedList(graph1, 8);
+            Console.WriteLine("DFS: ");
+           
+            Stack<int> DFSLinkedList = GraphLinkedList.DFS(X - 1, Y - 1);
+            ShowForStack(DFSLinkedList);
+            Console.WriteLine();
+            Console.WriteLine("BFS:");
+           
+            Stack<int> BFSLinkedList = GraphLinkedList.BFS(X - 1, Y - 1);
+            ShowForStack(BFSLinkedList);
+
+            Console.ReadKey();
+        }
+
+        public static void CheckInputCorrect(ref int i)
+        {
+            bool mark = false;
+            while (!mark)
+            {
+                try
+                {
+                    i = Convert.ToInt32(Console.ReadLine());
+                    if (i > 0 && i <= 8)
+                    {
+                        mark = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Введите верное значение");
+                    }
+                        
+                }
+                catch
+                {
+                    Console.WriteLine("Введите верное значение");
+                }
+            }
+        }
+
+        static void ShowForStack(Stack<int> stack)
+        {
+            int counter = 0;
+            foreach (int i in stack)
+            {
+                if(counter == 0)
+                {
+                    Console.Write(Convert.ToString(i + 1)); 
+                }
+                else
+                {
+                    Console.Write("->" + (i + 1));
+                }             
+                counter++;
+            }
+        }
+    }
+}
